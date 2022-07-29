4a8d93b149ffecd82461fc2a3f55b486144cef31
Ondra Pelech <ondra.pelech@gmail.com>
2022-07-29 03:09:59 +0200

    Fix 2.12, don't use ArraySeq

--- ssl/src/main/scala/org/scalanative/ssl/OpenSSLSessionContext.scala
+++ ssl/src/main/scala/org/scalanative/ssl/OpenSSLSessionContext.scala
@@ -20,21 +20,20 @@ import java.util.Enumeration
 import java.util.HashMap
 import javax.net.ssl.SSLSession
 import javax.net.ssl.SSLSessionContext
-import scala.collection.immutable.ArraySeq
 
 abstract class OpenSSLSessionContext extends SSLSessionContext {
 
-  private[this] val sessions = new HashMap[ArraySeq.ofByte, SSLSession]
+  private[this] val sessions = new HashMap[Array[Byte], SSLSession]
 
   override def getSession(sessionId: Array[Byte]): SSLSession =
-    sessions.get(ArraySeq.unsafeWrapArray(sessionId))
+    sessions.get(sessionId)
 
   override def getIds(): Enumeration[Array[Byte]] = {
     val iter = sessions.keySet().iterator()
     new Enumeration[Array[Byte]] {
       override def hasMoreElements(): Boolean = iter.hasNext()
       override def nextElement(): Array[Byte] =
-        iter.next().unsafeArray
+        iter.next()
     }
   }
 
